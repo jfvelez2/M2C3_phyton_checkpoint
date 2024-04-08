243625fc233a49b3237a87c9b970aa2c2a684c36
jfvelez2 <jfvelezsanta@gmail.com>
2024-04-08 02:03:08 +0200

    Phyton_Checkpoint-3

---
+++ Checkpoint_3.py
@@ -0,0 +1,47 @@
+# M2C3 Python Assignment
+
+# Exercise 1
+name_string = 'John'
+age_number = 39
+family_list = ['Emmanuel', 'Elena', 'Evan']
+assignment_complete = True
+
+#Exercise 2
+idx = name_string[0:3]
+print(idx)
+
+#Exercise 3
+first_element = family_list[0]
+print(first_element)
+
+#Exercise 4
+new_number = age_number + 10
+print(new_number)
+
+#Exercise 5
+last_element = family_list[2]
+print(last_element)
+
+#Exercise 6
+names = 'harry,alex,susie,jared,gail,conner'
+list_of_names = names.split(',')
+print(list_of_names)
+
+#Exercise 7
+idx_name = names[0:5].upper()
+new_names = idx_name + names[5:]
+print(new_names)
+
+#Exercise 8
+your_age = f"Hello, I'm {name_string} and I'm {age_number} years old"
+print(your_age)
+
+#Exercise 9
+print('hello world')
+
+#Exercise 10
+sentence = 'Hola esta es la tarea adicional'
+saludo = sentence[0:4]
+sentence = sentence.replace('Hola', 'adiós')
+print(saludo)
+print(sentence)
